fdf75edf5fbd230c6c846535bdd98560dc89ff58
Omir11 <141256351+Omir11@users.noreply.github.com>
2023-08-02 19:34:18 +0300

    Add files via upload

---
+++ TodoListAPI/todos/views.py
@@ -0,0 +1,54 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.http import Http404
+from .models import Todo
+from .serializers import TodoSerializer
+from rest_framework.pagination import PageNumberPagination
+
+class TodoListAPIView(APIView):
+    def get(self, request):
+        search_query = request.query_params.get('id')
+        if search_query:
+            todos = Todo.objects.filter(id=search_query)
+        else:
+            todos = Todo.objects.all()
+        serializer = TodoSerializer(todos, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = TodoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class TodoDetailAPIView(APIView):
+    def get_object(self, pk):
+        try:
+            return Todo.objects.get(pk=pk)
+        except Todo.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        todo = self.get_object(pk)
+        serializer = TodoSerializer(todo)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        todo = self.get_object(pk)
+        serializer = TodoSerializer(todo, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        todo = self.get_object(pk)
+        todo.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+class CustomPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 100
