adc36afc6c4aeccb5ff572f6f8342b7c55cfdf76
Omir1212 <omir.hosny1212@gmail.com>
2023-08-07 23:33:31 +0300

    Updated to Meet all Requirements

--- TodoListAPI/todos/views.py
+++ TodoListAPI/todos/views.py
@@ -1,54 +1,21 @@
-from rest_framework.views import APIView
+from rest_framework.mixins import ListModelMixin , CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin
+from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 from rest_framework import status
-from django.http import Http404
+from django.shortcuts import get_object_or_404
 from .models import Todo
-from .serializers import TodoSerializer
-from rest_framework.pagination import PageNumberPagination
+from .serializers import TodoSerializers
 
-class TodoListAPIView(APIView):
-    def get(self, request):
-        search_query = request.query_params.get('id')
-        if search_query:
-            todos = Todo.objects.filter(id=search_query)
-        else:
-            todos = Todo.objects.all()
-        serializer = TodoSerializer(todos, many=True)
-        return Response(serializer.data)
 
-    def post(self, request):
-        serializer = TodoSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class TodoDetailAPIView(RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin, GenericAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializers
 
-class TodoDetailAPIView(APIView):
-    def get_object(self, pk):
-        try:
-            return Todo.objects.get(pk=pk)
-        except Todo.DoesNotExist:
-            raise Http404
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
 
-    def get(self, request, pk):
-        todo = self.get_object(pk)
-        serializer = TodoSerializer(todo)
-        return Response(serializer.data)
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
 
-    def put(self, request, pk):
-        todo = self.get_object(pk)
-        serializer = TodoSerializer(todo, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk):
-        todo = self.get_object(pk)
-        todo.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-class CustomPagination(PageNumberPagination):
-    page_size = 10
-    page_size_query_param = 'page_size'
-    max_page_size = 100
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
